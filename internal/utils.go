9b57ab8ba2582b9ff5964bc7cb996d5acf60b71b
kot <cadyrov@gmail.com>
2022-03-21 19:35:44 +0500

    lint

--- internal/utils.go
+++ internal/utils.go
@@ -5,12 +5,13 @@ import (
 	"strings"
 )
 
+// nolint:staticcheck
 func sqlToGo(name string) string {
 	words := strings.Split(name, "_")
 	res := ""
 
 	for i := range words {
-		w := strings.ToTitle(words[i])
+		w := strings.Title(words[i])
 
 		if strings.EqualFold(w, "id") || strings.EqualFold(w, "url") || strings.EqualFold(w, "sql") ||
 			strings.EqualFold(w, "html") || strings.EqualFold(w, "http") || strings.EqualFold(w, "os") ||
